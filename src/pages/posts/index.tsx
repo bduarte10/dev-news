30f0159735cb83715cc88624853da47ca51f6232
Bruno Duarte <duartebruno2208@gmail.com>
2022-07-18 17:19:38 -0300

    :construction: Fetching data from Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -7,8 +7,18 @@ export default function Posts() {
       <Head>
         <title>Posts | dev.news</title>
       </Head>
-      <main>
-        <div>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href=''>
+            <time>12 de abril de 2033</time>
+            <strong>Creating something bla bla bla</strong>
+            <p>In this paragraph I start to writing some text</p>
+          </a>
+          <a href=''>
+            <time>12 de abril de 2033</time>
+            <strong>Creating something bla bla bla</strong>
+            <p>In this paragraph I start to writing some text</p>
+          </a>
           <a href=''>
             <time>12 de abril de 2033</time>
             <strong>Creating something bla bla bla</strong>
