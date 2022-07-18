449472e4d9943dda593e6c517b395174e6b5280c
Bruno Duarte <duartebruno2208@gmail.com>
2022-07-18 16:11:34 -0300

    :construction: Posts page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | dev.news</title>
+      </Head>
+      <main>
+        <div>
+          <a href=''>
+            <time>12 de abril de 2033</time>
+            <strong>Creating something bla bla bla</strong>
+            <p>In this paragraph I start to writing some text</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
