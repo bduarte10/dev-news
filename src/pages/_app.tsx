0dc3cea2796cf5274e16c98ddb078b99d8fd998a
Bruno Duarte <duartebruno2208@gmail.com>
2022-07-20 14:31:04 -0300

    :construction: Fetching data from Prismic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/global.scss';
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import { SessionProvider as NextAuthProvider } from 'next-auth/react';
+import { PrismicProvider } from '@prismicio/react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <NextAuthProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
-    </NextAuthProvider>
+    <PrismicProvider>
+      <NextAuthProvider session={session}>
+        <Header />
+        <Component {...pageProps} />
+      </NextAuthProvider>
+    </PrismicProvider>
   );
 }
 
